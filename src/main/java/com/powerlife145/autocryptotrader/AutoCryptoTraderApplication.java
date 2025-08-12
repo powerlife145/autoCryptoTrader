c02330bebca6c1577189be4f294c6a4c058d20e7
powerlife145 <powerlife145@gmail.com>
2025-08-12 11:07:39 +0900

    first main q

---
+++ src/main/java/com/powerlife145/autocryptotrader/AutoCryptoTraderApplication.java
@@ -0,0 +1,13 @@
+package com.powerlife145.autocryptotrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCryptoTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCryptoTraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powerlife145/autocryptotrader/AutoCryptoTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerlife145.autocryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCryptoTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
